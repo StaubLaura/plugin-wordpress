4182499af7d5dad61d1fb2d09f2935aec5f78837
Laura Staub <laura.staub@divcom.ch>
2019-05-20 10:47:00 +0200

    fonction qui remplace et par &

--- mon-premier-plugin.php
+++ mon-premier-plugin.php
@@ -31,14 +31,14 @@ add_action('delete_post', 'mon_plugin_post_delete_mail');
 
 
 //fonction qui remplace la chaine 'et' par '&amp;'
-function mon_plugin_the_title($title){
+//function mon_plugin_the_title($title){
 //Remplace 'et' dans le titre
         $title = str_replace( 'et', '&amp;', $title );
 //Retourne le titre modifié
         return $title;
     }
 //Ajout d'un filtre sur 'the_title' qui appellera mon_plugin_the_title()
-    add_filter( 'the_title', 'mon_plugin_the_title' );
+    //add_filter( 'the_title', 'mon_plugin_the_title' );
 
 
 //updload
