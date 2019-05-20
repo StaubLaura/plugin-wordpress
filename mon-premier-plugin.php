7be8802515aa12bf1d5372329e87ee5f4dbf08f3
Laura Staub <laura.staub@divcom.ch>
2019-05-20 10:53:36 +0200

    fonction qui remplace et par &

--- mon-premier-plugin.php
+++ mon-premier-plugin.php
@@ -15,18 +15,18 @@ function mon_plugin_meta_keywords() {
     echo '<meta name="keywords" content="HTML, CSS, XML, JavaScript">';
 }
 
-//fonction qui envoie par email les infos d'un email supprimé
+//Fonction qui envoie par email les infos d'un email supprimé
 function mon_plugin_post_delete_mail($post_id) {
-    //récupération informations de l'article supprimé
+//Récupére les informations de l'article supprimé
     $post = get_post($post_id);
-    //création sujet de l'email
-    $sujet = "Article supprimé : " .$post->post_title;
-    //création contenu de l'email
-    $message = "Contenu de l'article :" .$post->post_content;
-    //envoie email administrateur du site
+//Création du sujet de l'email
+    $sujet = "Artile supprimé :" . $post->post_title;
+//Création du contenu de l'email
+    $message = "Contenu de l'artilce : " . $post->post_content;
+//Envoi de l'email à l'administrateur du site
     wp_mail(get_bloginfo('admin_email'), $sujet, $message);
 }
-//ajout action sur 'delete_post' qui appellera mon_plugin_post_delete_mail()
+//Ajout d'une action sur 'delete_post' qui appellera mon_plugin_post_delete_mail()
 add_action('delete_post', 'mon_plugin_post_delete_mail');
 
 
@@ -40,5 +40,3 @@ add_action('delete_post', 'mon_plugin_post_delete_mail');
 //Ajout d'un filtre sur 'the_title' qui appellera mon_plugin_the_title()
     //add_filter( 'the_title', 'mon_plugin_the_title' );
 
-
-//updload
